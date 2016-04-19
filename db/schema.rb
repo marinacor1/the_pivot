6cfe84879ba85af8c3a81efc587362202d888d8f
thomp <thompickett@gmail.com>
2016-04-19 12:57:34 -0600

    Extract logic from views
    -change project directory name for new project
    - made a list helper file for cycling through lists
        - contains opening and closing div methods
    - made partial views for divs
    - made partial views for list
    - rendered partials in teams/index.html.erb
    - rendered partials in coders/index.html.erb
    - all tests pass
    - web pages still browse well
    - delete throw.rb

--- db/schema.rb
+++ db/schema.rb
@@ -32,5 +32,17 @@ ActiveRecord::Schema.define(version: 20160419170032) do
 
   add_index "coders", ["category_id"], name: "index_coders_on_category_id", using: :btree
 
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+  end
+
+  add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
+
   add_foreign_key "coders", "categories"
+  add_foreign_key "items", "categories"
 end
