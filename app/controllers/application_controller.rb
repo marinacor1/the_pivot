54858e57ca6cde8e42fdae2897127b67f087d82d
Sal Espinosa <sespinos@gmail.com>
2016-04-19 07:30:03 -0600

    Add initial styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_cart
-  
+  before_action :set_cart, :set_categories
+
   def set_cart
     @cart = Cart.new(session[:cart])
+  end
+
+  def set_categories
+    @categories = Category.all
   end
 end
