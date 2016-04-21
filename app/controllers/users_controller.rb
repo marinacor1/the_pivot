288aafa96b995387ec8d9f314bbcaffc8c308c03
Sal Espinosa <sespinos@gmail.com>
2016-04-20 20:11:12 -0600

    Revise formatting for site
    * Remove bootstrap and materialize.
    * Update
        * nave
        * secondary nav
        * home
        * coder lists
        * forms
        * flashes
        * root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:action] = "You've successfully logged in"
+      flash[:message] = "You've successfully logged in"
       redirect_to root_path
     else
       flash[:action] = "login unsuccesful"
