13da2f6328bc29c6ab684596b12b561cd5817db7
Hedy <hedywoo@gmail.com>
2016-04-23 21:34:08 -0600

    Implement update for admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,23 @@ class Admin::UsersController < Admin::BaseController
   def show
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:message] = "Profile successfully updated."
+      redirect_to admin_dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :organization, :password)
+  end
 end
