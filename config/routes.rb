4d44ecb912751550d86607491c7e9cc2d567e40f
Sal Espinosa <sespinos@gmail.com>
2016-05-04 20:45:56 -0600

    Revise routes using verbs instead of resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
-  root "homes#show"
-  resources :homes, only: [:show]
-  resources :coders, only: [:index, :show]
-  resources :users, only: [:new, :create]
-  resources :contracts, only: [:create, :show]
-  resources :teams, only: [:create, :index, :destroy]
+  root   "homes#show"
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  get "/dashboard", to: "users#show", as: :dashboard
-  get "/history", to: "contracts#index"
+  get    "/homes/:id",             to: "homes#show"
+  get    "/coders/:id",            to: "coders#show",           as: :coder
+  get    "/coders",                to: "coders#index",          as: :coders
+  get    "/users/new",             to: "users#new",             as: :new_user
+  post   "/users",                 to: "users#create",          as: :users
+  post   "/contracts",             to: "contracts#create",      as: :contracts
+  get    "/contract/:id",          to: "contracts#show",        as: :contract
+  get    "/teams",                 to: "teams#index",           as: :teams
+  post   "/teams",                 to: "teams#create"
+  delete "/teams/:id",             to: "teams#destroy",         as: :team
 
-  namespace :admin do
-    get "/dashboard", to: "users#show", as: :dashboard
-    resources :users, only: [:edit, :update]
-    resources :contracts, only: [:show, :index]
-    resources :coders, only: [:index, :edit, :update]
-  end
+  get    "/admin/dashboard",       to: "admin/users#show",      as: :admin_dashboard
+  get    "/admin/users/:id/edit",  to: "admin/users#edit",      as: :edit_admin_user
+  patch  "/admin/users/:id",       to: "admin/users#update",    as: :admin_user
+  get    "/admin/contracts/:id",   to: "admin/contracts#show",  as: :admin_contract
+  get    "/admin/contracts",       to: "admin/contracts#index", as: :admin_contracts
+  get    "/admin/coders",          to: "admin/coders#index",    as: :admin_coders
+  get    "/admin/coders/:id/edit", to: "admin/coders#edit",     as: :edit_admin_coder
+  patch  "/admin/coders/:id",      to: "admin/coders#update",   as: :admin_coder
 
-  get "/:slug", to: "categories#show", as: :category
+  get    "/login",                 to: "sessions#new"
+  post   "/login",                 to: "sessions#create"
+  delete "/logout",                to: "sessions#destroy"
+  get    "/dashboard",             to: "users#show",            as: :dashboard
+  get    "/history",               to: "contracts#index"
+
+  get    "/:slug",                 to: "categories#show",       as: :category
 end
