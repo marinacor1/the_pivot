58b428b59b0f06800ff2fb631b66f741f2ba976b
Admir Draganovic <admir.draganovic@gmail.com>
2016-05-10 13:34:48 -0600

    Add pending test
    Changed secrets file

--- spec/features/guest_can_add_multiple_reservations_to_cart_spec.rb
+++ spec/features/guest_can_add_multiple_reservations_to_cart_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'guest can add multiple reservations to cart' do
-  scenario 'they see total in cart after adding trips' do
+  xscenario 'they see total in cart after adding trips' do
     home1 = Home.create(address: "123 Fake Street", zip_code: "80002", title: "Adobe Mansion", description: "A basement with dirt walls.", image_url: "https://upload.wikimedia.org/wikipedia/commons/8/80/StoneBrick_Basement.JPG", cities_id: city1.id, user_id: user1.id)
     home2 = Home.create(address: "555 Street", zip_code: "80002", title: "Adobe Mansion", description: "A basement with dirt walls.", image_url: "https://upload.wikimedia.org/wikipedia/commons/8/80/StoneBrick_Basement.JPG", cities_id: city2.id, user_id: user2.id)
 
