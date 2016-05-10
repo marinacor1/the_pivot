0a5ea4aa4931026ad13c0687dd99b62c2de3c828
marinacor1 <marinaspam1234@gmail.com>
2016-05-10 16:42:11 -0600

    Adding initial seed methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_team, :nav_categories
   helper_method :current_user
-
-  def set_team
-    @team = Team.new(session[:team])
-  end
-
-  def nav_categories
-    @nav_categories = Category.all
-  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def initialize
+    generate_cities
+    generate_homes
+    generate_users
+    generate_dates
+  end
 
+  def generate_cities
+    10.times do
+      city = City.create!(name: Faker::Address.city,
+                          state: Faker::Address.state_abbr)
+    end
+  end
 
-Category.create!(name: "UI/UX")
-Category.create!(name: "Data Science ")
-Category.create!(name: "Full Stack Engineering")
-Category.create!(name: "Mobile Specialist")
-Category.create!(name: "DevOps")
-Category.create!(name: "Machine Learning")
+  def generate_homes
+    20.times do |i|
+      home = Home.create!(address: Faker::Address.street_address,
+                          image_url: "http://placekitten.com/648/#{i}",
+                          zip_code: Faker::Address.zip_code,
+                          title: Faker::Hipster.sentence(2),
+                          description: Faker::Hipster.sentence,
+                          daily_rate: 40.30)
+    end
+  end
 
-experience_array = ["Began career as a theoretical physicist at MIT, but after creating a widely-used system for data analysis, realized that building apps for humans was more rewarding that academic research.", "Spent the last five years designing developing and marketing products for companies ranging from game studios to startups to Fortune 100 companies.", "Expertise in building educational frameworks for the past five years", "Worked in the London-based Google DeepMind team developing the most advanced AI to date, using the highly complex Monaco Algorithm.", "Consummate learner of all things.", "Active in data science and open source communities.", "Coauthored the original portfolio management and testing packages for R: 'portfolio' (the first R package to include a treemap function) and 'backtest'.", "Prior to becoming a Super Coder, worked at Microsoft and CapitalOne as an architect to their big data systems.", "Passionate about multivariable calculus and linear algebra, and interned as a particle physicist at the CERN before life as a Super Coder.", "Past work building the neural network that learns how to play video games in a fashion similar to that of human, as well as a neural network that may be able to access an external memory like a conventional Turing machine.", "Worked at University College London's Gatsby Computational Neuroscience Unit, building machines with neural network that access external memory like a conventional Turing machine, resulting in a computer that mimics the short-term of a human brain.", "Wants to wonk to 'solve intelligence,' by trying to achieve the best techniques from machine learning and systems neuroscience to build powerful general-purpose learning algorithms.", "A child chess prodigy who won the Pentamind championship at the Mind Sports Olympiad five times, and now works to solve intelligene." ]
+  def generate_users
 
-cost_array = ["120000.00", "125000.00", "127500.00", "130000.00", "132500.00", "135000.00", "137000.00", "139000.00", "140000.00", "145000.00", "100000.00", "125000.00", "200000.00", "225000.00", "500000.00"]
+  end
 
-50.times do
-  Coder.create!(name: Faker::Name.name[0][0] + " "  + Faker::Name.name.split[1],
-                experience: experience_array.sample(2).join(" "),
-                cost: cost_array.sample,
-                image_url: "http://api.randomuser.me/portraits/thumb/#{['men', 'women'].sample}/#{rand(96)}.jpg",
-                category: Category.all.order("Random()").first)
+  def generate_dates
+    50.times do |i|
+      Date.create(date: Faker::Date.forward(i))
+    end
+  end
+
 end
