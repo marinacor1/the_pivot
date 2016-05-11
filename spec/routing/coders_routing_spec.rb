78c01397b9e282ebd478d2b3a1e8992e6f8cff32
Matt Pindell <pindell.m@gmail.com>
2016-05-11 13:48:21 -0600

    Remove old Model and Routing tests

--- spec/routing/coders_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe CodersController do
-  describe 'routing' do
-    it "routes to #index" do
-      expect(get("/coders")).to route_to("coders#index")
-    end
-  end
-end
