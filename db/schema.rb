2a0e01bca34c9f1347d596a90411ab4b888c68df
marinacor1 <marinaspam1234@gmail.com>
2016-05-12 15:50:16 -0600

    Create User Role model and table with reference to user and role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512214705) do
+ActiveRecord::Schema.define(version: 20160512214931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,16 @@ ActiveRecord::Schema.define(version: 20160512214705) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_roles", ["role_id"], name: "index_user_roles_on_role_id", using: :btree
+  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -90,4 +100,6 @@ ActiveRecord::Schema.define(version: 20160512214705) do
   add_foreign_key "reservation_days", "days"
   add_foreign_key "reservation_days", "reservations"
   add_foreign_key "reservations", "users"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
