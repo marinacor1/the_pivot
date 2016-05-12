31229a450c09620f23c7ac775e4f68864008bb82
marinacor1 <marinaspam1234@gmail.com>
2016-05-11 21:32:38 -0600

    Initial test has minor changes from original

---
+++ spec/features/registered_user_can_see_past_trips_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+feature "User can see individual past trips details" do
+  scenario "when they click link from history page" do
+    @user1, @user2 = create_list(:user, 2)
+    user = create(:user)
+    visit login_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Login"
+    visit users_path
+    @submissions = page.all('input[type="submit"]')
+    @submissions[0].click
+    @submissions[1].click
+    visit teams_path
+    click_link "Create Team"
+    @contract = Contract.first
+
+    formatted_date_created = @contract.created_at.strftime("%B %d, %Y - %H:%M")
+
+    visit history_path
+
+    expect(page).to have_content("Contract ID: #{@contract.id}")
+    click_link "View"
+
+    expect(page).to have_link(@user1.name)
+    expect(page).to have_link(@user2.name)
+    expect(page).to have_content(@user1.cost)
+    expect(page).to have_content(@user2.cost)
+    expect(page).to have_content("Contracted")
+    total_cost = sprintf("%.2f", (@user1.cost.to_f + @user2.cost.to_f))
+    expect(page).to have_content(total_cost)
+    expect(page).to have_content(formatted_date_created)
+    expect(page).to_not have_content("Completed at:")
+    expect(page).to_not have_content("Cancelled at:")
+  end
+
+  scenario "when they click link from history page and a contract has been completed" do
+    @contract.update(status: "Completed")
+    formatted_date_updated = @contract.updated_at.strftime("%B %d, %Y - %H:%M")
+
+    visit history_path
+    click_link "View"
+
+    expect(page).to have_content("Completed")
+    expect(page).to have_content("Completed at:")
+    expect(page).to have_content(formatted_date_updated)
+  end
+
+  scenario "when they click link from history page and a contract has been cancelled" do
+    @contract.update(status: "Cancelled")
+    formatted_date_updated = @contract.updated_at.strftime("%B %d, %Y - %H:%M")
+
+    visit history_path
+    click_link "View"
+
+    expect(page).to have_content("Cancelled")
+    expect(page).to have_content("Cancelled at:")
+    expect(page).to have_content(formatted_date_updated)
+  end
+
+  scenario "when they click link from history page and a contract has been cancelled" do
+    @contract.update(status: "Paid")
+
+    visit history_path
+    click_link "View"
+
+    expect(page).to have_content("Paid")
+  end
+end
