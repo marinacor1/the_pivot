b24bf8189abcbc44bc308b98e2c5b7f4a32eae97
marinacor1 <marinaspam1234@gmail.com>
2016-05-11 20:59:46 -0600

    Fix factories for user so they are no longer symbol

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -10,10 +10,10 @@ FactoryGirl.define do
   end
 
   factory :user do
-    :first_name Faker::Name.first_name
-    :last_name Faker::Name.last_name
-    :email Faker::Internet.email
-    :password "password"
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email Faker::Internet.email
+    password "password"
   end
 
   factory :city do
