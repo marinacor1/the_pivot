11737838d070af5ed81ef3aabd83de8d3e22309a
Matt Pindell <pindell.m@gmail.com>
2016-05-12 16:54:54 -0600

    Add ability to join ReservationDays to Reservation during ReservationController #create

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::ReservationsController < ApplicationController
 
   def create
+    start_date = reservation_params[:startDate].to_date
+    end_date   = reservation_params[:endDate].to_date
+    total_days = (end_date - start_date).to_i
+    home_id    = reservation_params[:homeId].to_i
+
+    # create day objects based off of range
+    # create ReservationDays (find days in db)
+    reservation = Reservation.new(home_id: home_id)
+    reserved_days = []
+    raw_date = start_date
+    (total_days + 1).times do
+      formatted_date = raw_date.strftime("%Y-%m-%d")
+      day_id         = Day.find_by(date: formatted_date).id
+      reserved_days << ReservationDay.create(day_id: day_id)
+      raw_date = raw_date.next
+    end
+
+    reservation.reservation_days << reserved_days
+    reservation.save
+    
+    # require "pry"
+    # binding.pry
+
   end
 
   private
