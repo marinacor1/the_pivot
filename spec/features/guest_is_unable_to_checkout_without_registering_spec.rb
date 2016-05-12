e69261cb0deca0e0fc5a42cb82d31132d78d3e9d
marinacor1 <marinaspam1234@gmail.com>
2016-05-11 21:34:33 -0600

    Remove edge case testing

--- spec/features/guest_is_unable_to_checkout_without_registering_spec.rb
+++ spec/features/guest_is_unable_to_checkout_without_registering_spec.rb
@@ -2,27 +2,6 @@ require 'rails_helper'
 
 feature 'Guest is unable to checkout without registering' do
   scenario 'must create a user account' do
-    user1, user2 = create_list(:user, 2)
-
-    visit users_path
-
-    submissions = page.all('input[type="submit"]')
-
-    submissions[0].click
-    submissions[1].click
-
-    visit teams_path
-
-    expect(page).to_not have_link("Create Team")
-
-    within("div.book") do
-      click_link("Create Account")
-    end
-
-    expect(current_path).to eq(new_user_path)
-  end
-
- scenario "must login to user account" do
      user1, user2 = create_list(:user, 2)
     city = create(:city_with_homes, name: "Denver", state: "CO")
     home = city.homes.first
@@ -42,5 +21,6 @@ feature 'Guest is unable to checkout without registering' do
     end
 
     expect(current_path).to eq(login_path)
-  end
+    end
+
 end
