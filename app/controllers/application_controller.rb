884474b246db20e8be539caa5495f4409d2037ea
marinacor1 <marinaspam1234@gmail.com>
2016-05-13 07:22:10 -0600

    Add authorize and current permission methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   helper_method :current_user
+  before_action :authorize!
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -9,4 +10,15 @@ class ApplicationController < ActionController::Base
   def set_cart
     @cart = Cart.new(session[:cart])
   end
+  
+  def current_permission
+    @current_permission ||=  PermissionsService.new(current_user, params[:controller], params[:action])
+  end
+
+  def authorize!
+    unless current_permission.allow?
+      redirect_to root_url, danger: "You shall not pass!"
+    end
+  end
+
 end
