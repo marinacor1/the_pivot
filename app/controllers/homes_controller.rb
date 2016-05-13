1822a09007306b300ea7554d1eb5e36c15bd1e06
marinacor1 <marinaspam1234@gmail.com>
2016-05-12 22:34:07 -0600

    Add flash errors for updated homes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,19 @@ class HomesController < ApplicationController
     @home = Home.find(params[:id])
   end
 
+  def update
+    if @home.update(params_check)
+      flash[:error] = "Success! Your home updated."
+      redirect_to @home
+    else
+      flash[:error] = "Your home could not be updated. Please check your input and try again."
+    end
+  end
+
+
+  private
+
+  def params_check
+    params.require(:home).permit(:image_url, :address, :zip_code, :title, :description, :daily_rate)
+  end
 end
