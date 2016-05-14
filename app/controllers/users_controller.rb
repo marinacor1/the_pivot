0116b38927cc4c7986ce84c5f521dfec475f2689
marinacor1 <marinaspam1234@gmail.com>
2016-05-14 13:32:28 -0600

    Add edit and update actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(params_check)
+      flash[:error] = "Success! Your account updated."
+    else
+      flash[:error] = "Your account could not be updated. Please check your input and try again."
+    end
+      redirect_to  dashboard_path
+  end
+
+
   def create
     @user = User.new(user_params)
     if @user.save
