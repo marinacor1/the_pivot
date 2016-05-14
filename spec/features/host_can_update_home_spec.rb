3b412860d2131cab60bfb2cc3b0d1555f875ee49
marinacor1 <marinaspam1234@gmail.com>
2016-05-14 14:42:49 -0600

    Clean up spacing and excess variables within host can update home spec

--- spec/features/host_can_update_home_spec.rb
+++ spec/features/host_can_update_home_spec.rb
@@ -5,12 +5,8 @@ RSpec.feature "host can update home" do
     city = create(:city_with_homes, name: "Denver", state: "CO")
     host = create(:user, email: "macies@li.biz", password: "password")
     home = city.homes.first
-
     original_home_name = home.title
     host_role = Role.create(name: "host")
-    # r_u_role = Role.create(name: "registered_user")
-    # UserRole.create(user: host, role: host_role)
-    # host.user_roles.create(role: host_role)
     host.roles << host_role
     host.home = home
 
@@ -29,11 +25,11 @@ RSpec.feature "host can update home" do
 
     expect(current_path).to eq("/denver-co/homes/#{home.id}/edit")
 
-  within(".edit_home_form") do
-    fill_in "Title", with: "Stinky Outhouse"
-    fill_in "Description", with: "It's stinky. Very stinky."
-    click_on "Submit"
-  end
+    within(".edit_home_form") do
+      fill_in "Title", with: "Stinky Outhouse"
+      fill_in "Description", with: "It's stinky. Very stinky."
+      click_on "Submit"
+    end
 
     expect(page).to have_content "Stinky Outhouse"
     expect(page).to have_content "It's stinky. Very stinky."
