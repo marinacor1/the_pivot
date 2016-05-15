2c63de9a03e995003b7c728b37f30ac08f9a4d53
Matt Pindell <pindell.m@gmail.com>
2016-05-14 21:29:26 -0600

    Refactor / format test

--- spec/features/user_can_book_any_home_spec.rb
+++ spec/features/user_can_book_any_home_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature "User can book any home" do
   scenario "when view a home show page", js: true do
     user = create(:user)
-
     city = create(:city_with_homes, name: "Denver", state: "CO")
     home = city.homes.first
 
@@ -11,14 +10,15 @@ RSpec.feature "User can book any home" do
 
     visit '/denver-co'
 
+    within('#cart-count') do
+      expect(page).to have_text("0")
+    end
+
     click_on home.title
 
     expect(current_path).to eq("/denver-co/homes/#{home.id}")
 
     within(".date-picker-box") do
-      # fill_in('.form-control', with: "07/23/2016 - 07/25/2016")
-      # page.execute_script("$('#home-date-range').datepicker('startDate', '01/01/2010')")
-
       expect(page).to have_button("Reserve Dates")
       sleep(1)
       click_button "Reserve Dates"
@@ -27,10 +27,11 @@ RSpec.feature "User can book any home" do
     within('#lets-go') do
       sleep(1)
       click_on "Let's Go!"
+      sleep(1)
     end
 
-    expect(page).to have_text("Cart 1")
-    # expect cart to increase by one / modal?
-    # expect(page).to have_content "Pack your bags! Your reservations are made!"
+    within('#cart-count') do
+      expect(page).to have_text("1")
+    end
   end
 end
