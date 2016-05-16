f24e923fb41551d719f3ab9165dd4ecd228c058a
marinacor1 <marinaspam1234@gmail.com>
2016-05-16 16:18:53 -0600

    Adding x path

--- spec/features/platform_admin_can_edit_user_spec.rb
+++ spec/features/platform_admin_can_edit_user_spec.rb
@@ -10,10 +10,24 @@ RSpec.feature "platform admin can edit any user" do
     home.users << host
     platform_admin = create(:user, email: "pa@admin.co", password: "password")
     platform_admin.roles << Role.create(name: "platform_admin")
-    user1 = create(:user)
-    user2 = create(:user)
+    user1 = User.create(first_name: "Sam", last_name: "Watson", email: "emailz@hotmail.com", password: "password")
+    user1.roles << Role.create(name: "registered_user")
+    user2 = User.create(first_name: "Sally", last_name: "Watson", email: "email3@hotmail.com", password: "password")
+    user2.roles << Role.create(name: "registered_user")
+
+    visit root_path
+
+    click_link "Login"
+
+    expect(current_path).to eq '/login'
+    fill_in "email", with: "#{platform_admin.email}"
+    fill_in "password", with: "password"
+    click_button "Login"
 
     visit '/users'
+
+  find(:xpath, "//tr[contains(., 'Sally')]/td/a", :text => '#{user2.email}').click
+
 save_and_open_page
   end
 end
