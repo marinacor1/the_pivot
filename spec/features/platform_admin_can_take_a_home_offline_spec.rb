812b431a162268db29c098952473fd32fff559f9
marinacor1 <marinaspam1234@gmail.com>
2016-05-16 22:32:55 -0600

    Spec for platform can take home offline written

---
+++ spec/features/platform_admin_can_take_a_home_offline_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "host can update home" do
+  it "shows an update form for home" do
+    city = create(:city_with_homes, name: "Denver", state: "CO")
+    host = create(:user, email: "macies@li.biz", password: "password")
+    home = city.homes.first
+    host.roles << Role.create(name: "host")
+    home.users << host
+    platform_admin = create(:user, email: "pa@admin.co", password: "password")
+    platform_admin.roles << Role.create(name: "platform_admin")
+
+    visit root_path
+
+    click_link "Login"
+
+    expect(current_path).to eq '/login'
+    fill_in "email", with: "#{platform_admin.email}"
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    expect(current_path).to eq '/dashboard'
+
+    click_link "Edit User Account or Homes"
+
+    click_link "Edit User Home"
+
+    uncheck("Online?")
+
+    visit "city_home_path(#{home.city},#{home})"
+
+    expect(page).to have_content "We're sorry. This page is currently offline."
+  end
+end
