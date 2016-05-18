54c6c949492f9e7413f45349f83e52424d1c524a
marinacor1 <marinaspam1234@gmail.com>
2016-05-18 14:40:37 -0600

    Refactor platform admin can take a home offline modules

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -1,6 +1,5 @@
 module FeaturesHelper
 
-
   def user_login(user)
     visit root_path
 
@@ -14,6 +13,16 @@ module FeaturesHelper
     within(".login-user-form") do
       click_on "Login"
     end
+  end
+
+  def host_login(host)
+    click_link "Login"
+
+    expect(current_path).to eq '/login'
+    fill_in "email", with: "#{host.email}"
+    fill_in "password", with: "password"
+
+    click_button "Login"
   end
 
   def platform_admin_login(user)
