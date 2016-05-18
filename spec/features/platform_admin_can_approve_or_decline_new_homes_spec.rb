3dfa9596d6bb0ca5077dec9a0c88294f527969b3
marinacor1 <marinaspam1234@gmail.com>
2016-05-18 07:52:23 -0600

    Test has expectations for home value

--- spec/features/platform_admin_can_approve_or_decline_new_homes_spec.rb
+++ spec/features/platform_admin_can_approve_or_decline_new_homes_spec.rb
@@ -27,6 +27,14 @@ RSpec.feature "platform admin can approve or decline new homes" do
 
     expect(current_path).to eq dashboard_path
 
+    expect("Tiny LA Home").to eq Home.all.last.title
+    expect("My house is beautiful.").to eq Home.all.last.description
+    expect("https://s-media-cache-ak0.pinimg.com/736x/1e/c5/eb/1ec5eb28f7580c245d571fcb8be7560e.jpg").to eq Home.all.last.image_url
+    expect("123 Lane Street").to eq Home.all.last.address
+    expect("80203").to_s.to eq Home.all.last.zip_code
+    expect("$16").to eq Home.all.last.daily_rate
+    expect("Los Angeles").to eq Home.all.last.city.name
+
     expect(page).to have_content "Your request has been submitted for approval."
 
     click_link "Logout"
@@ -38,12 +46,12 @@ RSpec.feature "platform admin can approve or decline new homes" do
     expect(current_path).to eq dashboard_path
 
     click_link "Pending Homes"
-
+save_and_open_page
     check("Pending?")
     click_button "Submit"
 
     expect(current_path).to eq dashboard_path
-    
+
     visit city_home_path(Home.all.last)
 
     expect(page).to have_content "Tiny LA Home"
