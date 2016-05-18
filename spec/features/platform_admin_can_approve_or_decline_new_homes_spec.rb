c5768f012f6b413d9f3fc56ed0c9905484758d9a
marinacor1 <marinaspam1234@gmail.com>
2016-05-18 09:33:26 -0600

    Stop with from and pending page

--- spec/features/platform_admin_can_approve_or_decline_new_homes_spec.rb
+++ spec/features/platform_admin_can_approve_or_decline_new_homes_spec.rb
@@ -46,10 +46,11 @@ RSpec.feature "platform admin can approve or decline new homes" do
     expect(current_path).to eq dashboard_path
 
     click_link "Pending Homes"
+save_and_open_page
+    fill_in "Status:", with: "Accept"
 
-    check("Pending?")
     click_button "Submit"
-
+#TODO when platform admin checks pending? it will make the site online
     expect(current_path).to eq pending_homes_path
 
     visit city_home_path(Home.all.last)
