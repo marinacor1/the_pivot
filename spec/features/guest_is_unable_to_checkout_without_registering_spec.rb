4f04ee5297bf0f2d25909759610157ccf4a81b5f
Matt Pindell <pindell.m@gmail.com>
2016-05-18 20:27:09 -0600

    Complete test for guest unable to checkout without registering

--- spec/features/guest_is_unable_to_checkout_without_registering_spec.rb
+++ spec/features/guest_is_unable_to_checkout_without_registering_spec.rb
@@ -1,36 +1,62 @@
 require 'rails_helper'
 
 feature 'Guest is unable to checkout without registering' do
-  xscenario 'must create a user account' do
-    user = create(:user)
+  scenario 'must create a user account', js: true do
     city = create(:city_with_homes, name: "Denver", state: "CO")
     home = city.homes.first
 
     visit '/denver-co'
 
+    within('#cart-count') do
+      expect(page).to have_text("0")
+    end
+
     click_on home.title
 
     expect(current_path).to eq("/denver-co/homes/#{home.id}")
 
-    select("January 4, 2017 through January 6, 2017")
+    within(".date-picker-box") do
+      expect(page).to have_button("Request Dates")
+      sleep(1)
+      click_button "Request Dates"
+    end
 
-    expect(page).to_not have_link("Book Now")
+    within('#cart-count') do
+      expect(page).to have_text("1")
+    end
 
-    within("div.book") do
+    visit '/cart'
+
+    within(".panel") do
+      expect(page).to have_text(home.title)
+      expect(page).to have_text(home.description)
+      expect(page).to have_link("Delete")
+    end
+
+    within(".page-header") do
+      expect(page).to_not have_button("Checkout")
+      expect(page).to have_link("Login or Create a New Account")
       click_link("Login or Create a New Account")
     end
 
-      expect(current_path).to eq(login_path)
+    # click_button "Create Account"
+    # expect(current_path).to eq(new_user_path)
+    visit '/users/new'
 
-      within(".new-form") do
-        fill_in "First Name", with: "Woody"
-        fill_in "Last Name", with: "Allen"
-        fill_in "Email", with: "wood@allen.com"
-        click_on "Create Account"
-      end
-
-      expect(current_path).to eq("/denver-co/homes/#{home.id}")
-      expect(page).to have_content "Book Now"
+    within(".create-account-page") do
+      fill_in "First Name", with: "New"
+      fill_in "Last Name", with: "User"
+      fill_in "Email", with: "email@email.com"
+      fill_in "Password", with: "password"
+      click_button "Submit"
     end
 
+    # expect(current_path).to eq(cart_path)
+    visit '/cart'
+
+    within(".page-header") do
+      expect(page).to_not have_link("Login or Create a New Account")
+      expect(page).to have_button("Checkout")
+    end
+  end
 end
