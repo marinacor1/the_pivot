a68f7047b8c39c4bf61dd3d493b2a8f5e1d4c422
marinacor1 <marinaspam1234@gmail.com>
2016-05-26 13:10:17 -0600

    Add create action to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,11 +4,14 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.create(params_check)
-    if @review.update_attributes(params_check)
+    binding.pry
+    @review = Review.new(params_check)
+    if @review.save
+      binding.pry
       flash[:error] = "Your review has been submitted!"
     else
-      flash[:error] = "We're sorry but your review could not be submitted."
+      binding.pry
+      flash[:error] = "We're sorry, but your review could not be submitted."
     end
       redirect_to dashboard_path
   end
