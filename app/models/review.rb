37bdcae70f4bcd7c41522b14637f78aaae836ac3
marinacor1 <marinaspam1234@gmail.com>
2016-05-31 20:39:18 -0600

    Customer rake works

--- app/models/review.rb
+++
@@ -1,8 +0,0 @@
-class Review < ActiveRecord::Base
-  belongs_to :home
-  belongs_to :trip
-  has_one :reservation
-
-  validates :title, presence: true
-  validates :thoughts, presence: true
-end
--- db/migrate/20160527045904_add_reservation_id_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddReservationIdToReviews < ActiveRecord::Migration
-  def change
-    add_reference :reviews, :reservation, index: true, foreign_key: true
-  end
-end
